b2d2be4f14dc8edd433f594a9dd2d88aea73131d
nils <nils.lerin@gmail.com>
2014-03-03 22:10:57 +0100

    Fixed convertmap script, was not the entire map before

--- convertmap.py
+++ convertmap.py
@@ -8,7 +8,7 @@ values.read(fin, 4800) # read 1 integer
 
 out = open(r'E:\ExtDev\alpha\demo\myTerrainData.txt', "w")
 with out:
-	for c in range(80):
-		for r in range(60):
+	for c in range(60):
+		for r in range(80):
 			out.write( str(int(not bool(values[(r*60)+c])))+"," )
 		out.write("\n")
